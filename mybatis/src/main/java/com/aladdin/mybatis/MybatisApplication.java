4eb13b4f805b2ca15c2d89d8e30f228bbd3ae582
guangchangli <guangchangde@gmail.com>
2020-02-10 23:57:15 +0800

    todo MybatisApplication

--- mybatis/src/main/java/com/aladdin/mybatis/MybatisApplication.java
+++ mybatis/src/main/java/com/aladdin/mybatis/MybatisApplication.java
@@ -1,13 +1,16 @@
 package com.aladdin.mybatis;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MybatisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MybatisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(MybatisApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
+    }
 
 }
